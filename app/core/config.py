01d8c0a09e3febf1786c0d26fbb00c94fd5f8a6d
DCT <dheemanchaitanyathakur@gmail.com>
2026-06-26 00:16:54 +0530

    fixing: redis down issue

--- app/core/config.py
+++ app/core/config.py
@@ -6,7 +6,12 @@ class Settings(BaseSettings):
     ALGORITHM: str
     ACCESS_TOKEN_EXPIRE_MINUTES: int
     DATABASE_URL: str
+
+    REDIS_HOST: str = "localhost"
+    REDIS_PORT: int = 6379
+
     DEBUG: bool = False
+
     model_config = SettingsConfigDict(
         env_file=".env",
         extra="ignore"
