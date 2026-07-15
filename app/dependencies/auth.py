eb8d5f5d489577065cda62271d020c1ca3054915
Dheeman Chaitanya Thakur <dheemanchaitanyathakur@gmail.com>
2026-07-15 19:54:05 +0530

    Update token URL for OAuth2PasswordBearer

--- app/dependencies/auth.py
+++ app/dependencies/auth.py
@@ -3,7 +3,7 @@ from fastapi.security import OAuth2PasswordBearer
 from app.core.JWT_handler import verify_access_token
 
 oauth2_scheme = OAuth2PasswordBearer(
-    tokenUrl="auth/login"
+    tokenUrl="/api/v1/auth/login"
 )
 
 
